dfc7843537cde5400a0fb28783f02adb429134ad
Natalia Stewart <stewa165@mail.chapman.edu>
2015-11-11 06:22:03 +0000

    Yes more cache removal

--- examples.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-version = 0.1
-
-def fibiter(n):
-    """Fibonacci number iterator"""
-    old, new = 0, 1
-    for _ in xrange(n):
-        old, new = new, old + new
-        yield(old)  #yield temporarily returns the value but remembers where it is in the function
